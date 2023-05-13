710a1027bb60272a051b87213d665348b9dac97f
dwd9999 <ojw1010@gmail.com>
2023-05-13 20:13:27 +0900

    05/13 Constraint Layout

--- app/src/main/java/com/example/capstone_writers/MainActivity.kt
+++ app/src/main/java/com/example/capstone_writers/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.capstone_writers
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginBtn = findViewById<Button>(R.id.LoginBtn)
+        loginBtn.setOnClickListener {
+            val intent1 = Intent(this, LoginActivity::class.java)
+            startActivity(intent1)
+        }
+
+        val registerBtn = findViewById<Button>(R.id.RegisterBtn)
+        registerBtn.setOnClickListener {
+            val intent2 = Intent(this, RegisterActivity::class.java)
+            startActivity(intent2)
+        }
+
     }
 }
