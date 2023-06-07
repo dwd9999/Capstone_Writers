637b1852d2bf850e8dd2d2aff5829c632d4e390f
dodo <betimidtodie@naver.com>
2023-06-07 15:21:04 +0900

    6/7 test commit

---
+++ app/src/main/java/com/example/capstone_writers/DataModel.kt
@@ -0,0 +1,18 @@
+package com.example.capstone_writers
+
+data class DataModel (
+
+    val reqUsername : String = "",
+    val reqUseremail : String = "",
+    val reqUserphone : String = "",
+    val reqSize : String = "",
+    val reqExp : String = "",
+    val reqCharExp: String = "",
+    val appUseremail : String = "",
+    val appExpSum : String = "",
+    val appExpTime : String = "",
+    val appmemo : String = "",
+    val appwriterintro : String = "",
+
+
+        )
